a8a47a669dded3f2cf4a9e1c51e3568cc9f95c35
Tanaka Naoto <gta_youtube@yahoo.co.jp>
2019-03-02 14:45:48 +0900

    プロフィールアイコン用の画像ディレクトリを作成

--- lib/Define.php
+++ lib/Define.php
@@ -1,4 +1,5 @@
 <?php 
 define('PATH_ACCOUNT_JSON', './account.json');
+define('PATH_ICON_DIR', dirname(__FILE__) . '/../icons');
 define('PATH_USERNAMES_EXAMPLE', './storage/usernames_example.txt');
 
